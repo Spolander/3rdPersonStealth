0e48237c5859f383fd9384bd291e6d42eeb6ff6b
Spolander <38954068+Spolander@users.noreply.github.com>
2019-02-18 22:07:18 +0200

    new player model

---
+++ Assets/Scripts/OverTheShoulder.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using Cinemachine;
+
+/// <summary>
+/// An add-on module for Cinemachine Virtual Camera that offsets the camera's position
+/// </summary>
+[ExecuteInEditMode]
+[SaveDuringPlay]
+[AddComponentMenu("")] // Hide in menu
+public class OverTheShoulder : CinemachineExtension
+{
+    [Tooltip("Offset the camera's position by this much (camera space)")]
+    public Vector3 m_Offset = Vector3.zero;
+
+    protected override void PostPipelineStageCallback(
+        CinemachineVirtualCameraBase vcam,
+        CinemachineCore.Stage stage, ref CameraState state, float deltaTime)
+    {
+        // Apply after the camera has been aimed.
+        // To apply offset before the camera aims, change this to Body
+        if (stage == CinemachineCore.Stage.Aim)
+        {
+            state.PositionCorrection += state.FinalOrientation * m_Offset;
+        }
+    }
+}
